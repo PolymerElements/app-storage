c877cfbfc1ca48fd5fa644a9689ab45e12cdcfa8
Chris Joel <cdata@google.com>
2016-05-01 17:08:58 -0700

    Add support for session-specific mirroring.

--- carbon-indexeddb-mirror/carbon-indexeddb-mirror-worker.js
+++ carbon-indexeddb-mirror/carbon-indexeddb-mirror-worker.js
@@ -1,21 +1,24 @@
 (function() {
   'use strict';
 
+  const internalStoreName = 'internal';
+
   let clientPorts = Symbol('clientPorts');
   let dbName = Symbol('dbName');
   let storeName = Symbol('storeName');
 
   class CarbonIndexedDBMirrorWorker {
     constructor(_dbName='carbon-mirror', _storeName='mirrored_data') {
-      // Maybe useful in case we want to notify clients of changes..
       this[dbName] = _dbName;
       this[storeName] = _storeName;
+      // Maybe useful in case we want to notify clients of changes..
       this[clientPorts] = new Set();
       this.dbOpens = new Promise((resolve, reject) => {
         let request = indexedDB.open(_dbName);
 
         request.onupgradeneeded = () => {
           request.result.createObjectStore(_storeName);
+          request.result.createObjectStore(internalStoreName);
         };
         request.onsuccess = () => {
           resolve(request.result);
@@ -31,24 +34,18 @@
           'error', error => console.error(error));
       self.addEventListener(
           'message', message => this.handleGlobalMessage(message));
+
       console.log('CarbonIndexedDBMirrorWorker started...');
     }
 
-    transaction(method, key, value=null) {
-      switch(method) {
-        case 'get':
-          return this.get(key);
-        case 'set':
-          return this.set(key, value);
-      }
-    }
+    operateOnStore(operation, storeName, mode) {
+      let operationArgs = Array.from(arguments).slice(3);
 
-    get(key) {
       return this.dbOpens.then(db => {
         return new Promise((resolve, reject) => {
-          let transaction = db.transaction(this[storeName], 'readonly');
-          let store = transaction.objectStore(this[storeName]);
-          let request = store.get(key);
+          let transaction = db.transaction(storeName, mode);
+          let store = transaction.objectStore(storeName);
+          let request = store[operation].apply(store, operationArgs);
 
           transaction.oncomplete = () => resolve(request.result);
           transaction.onabort = () => reject(transaction.error);
@@ -56,16 +53,43 @@
       });
     }
 
-    set(key, value) {
-      return this.dbOpens.then(db => {
-        return new Promise((resolve, reject) => {
-          let transaction = db.transaction(this[storeName], 'readwrite');
-          let store = transaction.objectStore(this[storeName]);
-          let request = store.put(value, key);
+    get(storeName, key) {
+      return this.operateOnStore('get', storeName, 'readonly', key);
+    }
 
-          transaction.oncomplete = () => resolve(request.result);
-          transaction.onabort = () => resolve(transaction.error);
-        });
+    set(storeName, key, value) {
+      return this.operateOnStore('put', storeName, 'readwrite', value, key);
+    }
+
+    clear(storeName) {
+      return this.operateOnStore('clear', storeName, 'readwrite');
+    }
+
+    transaction(method, key, value=null) {
+      switch(method) {
+        case 'get':
+          return this.get(this[storeName], key);
+        case 'set':
+          return this.set(this[storeName], key, value);
+      }
+    }
+
+    validateSession(session) {
+      return Promise.all([
+        this.dbOpens,
+        this.get(internalStoreName, 'session')
+      ]).then(results => {
+        let db = results[0];
+        let currentSession = results[1];
+        let operations = [];
+
+        if (session !== currentSession) {
+          if (currentSession != null) {
+            operations.push(this.clear(this[storeName]));
+          }
+
+          operations.push(this.set(internalStoreName, 'session', session));
+        }
       });
     }
 
@@ -82,19 +106,26 @@
         return;
       }
 
-      switch(event.data.type) {
-        case 'carbon-mirror-transaction':
-          let transaction = this.transaction(
-              event.data.method, event.data.key, event.data.value);
-          let id = event.data.id;
+      let id = event.data.id;
 
-          transaction.then((result) => {
+      switch(event.data.type) {
+        case 'carbon-mirror-validate-session':
+          this.validateSession(event.data.session).then(() => {
             port.postMessage({
-              type: 'carbon-mirror-transaction-result',
-              id,
-              result
+              type: 'carbon-mirror-session-validated',
+              id
             });
           });
+          break;
+        case 'carbon-mirror-transaction':
+          this.transaction(event.data.method, event.data.key, event.data.value)
+              .then(result => {
+                port.postMessage({
+                  type: 'carbon-mirror-transaction-result',
+                  id,
+                  result
+                });
+              });
           break;
         case 'carbon-mirror-disconnect':
           this[clientPorts].remove(port);
