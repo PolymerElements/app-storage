cd99a3fcaedfe842b084acc1ed8046bbd2d6d9a1
Chris Joel <cdata@google.com>
2016-05-14 19:38:33 -0700

    📜 Document `<app-indexeddb-mirror>` and friends.
    Also includes miscellaneous ES6-to-ES5 conversion and some other cleanup
    of opportunity.

--- app-indexeddb-mirror/app-indexeddb-mirror-worker.js
+++ app-indexeddb-mirror/app-indexeddb-mirror-worker.js
@@ -1,3 +1,13 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
 (function() {
   'use strict';
 
@@ -10,11 +20,24 @@
 
   var MIGRATIONS = [
     // v1
-    function(context) {context.database.createObjectStore(context.__storeName)},
+    function(context) {
+      context.database.createObjectStore(context.__storeName);
+    },
     // v2
-    function(context) {context.database.createObjectStore(INTERNAL_STORE_NAME)}
+    function(context) {
+      context.database.createObjectStore(INTERNAL_STORE_NAME);
+    }
   ];
 
+  /**
+   * Class that implements a worker process negotiates connections from clients
+   * in other threads, and operates on an IndexedDB database object store.
+   *
+   * @param {string} _dbName The name of the IndexedDB database to create and
+   * open.
+   * @param {string} _storeName The name of the IndexedDB object store to use
+   * for storing values.
+   */
   function AppIndexedDBMirrorWorker(_dbName, _storeName) {
     _dbName = _dbName || 'app-mirror';
     _storeName = _storeName || 'mirrored_data';
@@ -47,13 +70,27 @@
     });
 
     self.addEventListener(
-        'unhandledrejection', function(error){ console.error(error) });
+        'unhandledrejection', function(error){ console.error(error); });
     self.addEventListener(
-        'error', function(error) { console.error(error) });
+        'error', function(error) { console.error(error); });
     console.log('AppIndexedDBMirrorWorker started...');
   };
 
   AppIndexedDBMirrorWorker.prototype = {
+
+    /**
+     * Perform a transaction on an IndexedDB object store.
+     *
+     * @param {string} operation The name of the method to call on the object
+     * store instance.
+     * @param {string} storeName The name of the object store to operate on.
+     * @param {string} mode The mode of the transaction that will be performed.
+     * @param {...} operationArg The arguments to call the method named by
+     * the operation parameter.
+     * @return {Promise} A promise that resolves when the transaction completes,
+     * with the result of the transaction, or rejects if the transaction fails
+     * with the error reported by the transaction.
+     */
     operateOnStore: function(operation, storeName, mode) {
       var operationArgs = Array.from(arguments).slice(3);
 
@@ -63,24 +100,60 @@
           var store = transaction.objectStore(storeName);
           var request = store[operation].apply(store, operationArgs);
 
-          transaction.oncomplete = function() {resolve(request.result)};
-          transaction.onabort = function() {reject(transaction.error)};
+          transaction.oncomplete = function() { resolve(request.result); };
+          transaction.onabort = function() { reject(transaction.error); };
         });
       });
     },
 
+    /**
+     * Perform a "get" operation on an IndexedDB object store.
+     *
+     * @param {string} storeName The name of the object store to operate on.
+     * @param {string} key The key in the object store that corresponds to the
+     * value that should be got.
+     * @return {Promise} A promise that resolves with the outcome of the
+     * operation.
+     */
     get: function(storeName, key) {
       return this.operateOnStore('get', storeName, 'readonly', key);
     },
 
+    /**
+     * Perform a "put" operation on an IndexedDB object store.
+     *
+     * @param {string} storeName The name of the object store to operate on.
+     * @param {string} key The key in the object store that corresponds to the
+     * value that should be put.
+     * @param {} value The value to be put in the object store at the given key.
+     * @return {Promise} A promise that resolves with the outcome of the
+     * operation.
+     */
     set: function(storeName, key, value) {
       return this.operateOnStore('put', storeName, 'readwrite', value, key);
     },
 
+    /**
+     * Perform a "clear" operation on an IndexedDB object store.
+     *
+     * @param {string} storeName The name of the object store to operate on.
+     * @return {Promise} A promise that resolves with the outcome of the
+     * operation.
+     */
     clear: function(storeName) {
       return this.operateOnStore('clear', storeName, 'readwrite');
     },
 
+    /**
+     * Performs a transaction (in the parlance of the the client).
+     *
+     * @param {string} method The method of the transaction. Supported methods
+     * are `"get"` and `"set"`.
+     * @param {string} key The key to get or set.
+     * @param {=} value The value to set, when the method is `"set"`.
+     * @return {Promise} A promise that resolves with the outcome of the
+     * transaction, or rejects if an unsupported method is attempted.
+     */
     transaction: function(method, key, value) {
       value = value || null;
 
@@ -90,8 +163,19 @@
         case 'set':
           return this.set(this[STORE_NAME], key, value);
       }
+
+      return Promise.reject(new Error('Method not supported: ' + method));
     },
 
+    /**
+     * Compares the currently stored session value to a provided session. If
+     * the provided session does not match the current session, the database
+     * is cleared and the provided session becomes the current session.
+     *
+     * @param {string} session A session to compare to the current session.
+     * @return {Promise} A promise that resolves with the validation process
+     * has completed and the session has been updated (if necessary).
+     */
     validateSession: function(session) {
       return Promise.all([
         this.dbOpens,
@@ -111,6 +195,13 @@
       }.bind(this));
     },
 
+    /**
+     * Registers a client, represented by a MessagePort. The port is
+     * presumed to be a direct, unshared channel to the client being registerd.
+     *
+     * @param {MessagePort} port The port that represents the client being
+     * registered.
+     */
     registerClient: function(port) {
       port.addEventListener('message', function(event) {
         this.handleClientMessage(event, port)
@@ -124,6 +215,15 @@
       port.postMessage({ type: 'app-mirror-connected' });
     },
 
+    /**
+     * Triages messages received from a specific client, dispatches their
+     * data to the appropriate methods and responds to the client if applicable.
+     *
+     * @param {MessageEvent} event The event that contains the message sent by
+     * the client.
+     * @param {MessagePort} port The port the represents the client the sent the
+     * message.
+     */
     handleClientMessage: function(event, port) {
       if (!event.data) {
         return;
