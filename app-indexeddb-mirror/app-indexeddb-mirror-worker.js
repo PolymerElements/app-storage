f26140471b0ffebef81b6f87f6bccbef86444f84
Elliott Marquez <emarquez@google.com>
2017-04-06 20:27:27 -0700

    code makes more sense got rid ifs

--- app-indexeddb-mirror/app-indexeddb-mirror-worker.js
+++ app-indexeddb-mirror/app-indexeddb-mirror-worker.js
@@ -51,6 +51,9 @@
     this[CLIENT_PORTS] = new Array;
     this[DB_OPENS] = null;
     this.supportsIndexedDB = self.indexedDB != null;
+    if (!self.Promise || !this.supportsIndexedDB) {
+      console.error('Indexeddb Mirror is not supported');
+    }
 
     this.openDb();
 
@@ -64,10 +67,6 @@
   AppIndexedDBMirrorWorker.prototype = {
 
     openDb: function() {
-      if (!self.Promise || !this.supportsIndexedDB) {
-        return;
-      }
-
       this.__dbOpens = this.__dbOpens || new Promise(function(resolve, reject) {
         console.log('Opening database..');
 
@@ -99,10 +98,6 @@
     },
 
     closeDb: function() {
-      if (!self.Promise || !this.supportsIndexedDB) {
-        return;
-      }
-
       if (this.__dbOpens == null) {
         return Promise.resolve();
       }
@@ -127,12 +122,12 @@
      * with the result of the transaction, or rejects if the transaction fails
      * with the error reported by the transaction.
      */
-    operateOnStore: function(operation, storeName, mode, operationArgs) {
+    operateOnStore: function(operation, storeName, mode) {
       if (!self.Promise || !this.supportsIndexedDB) {
         return;
       }
 
-      operationArgs = Array.from(arguments).slice(3);
+      var operationArgs = Array.from(arguments).slice(3);
 
       return this.openDb().then(function(db) {
 
@@ -202,10 +197,6 @@
      * transaction, or rejects if an unsupported method is attempted.
      */
     transaction: function(method, key, value) {
-      if (!self.Promise || !this.supportsIndexedDB) {
-        return;
-      }
-
       value = value || null;
 
       switch(method) {
@@ -228,10 +219,6 @@
      * has completed and the session has been updated (if necessary).
      */
     validateSession: function(session) {
-      if (!self.Promise || !this.supportsIndexedDB) {
-        return;
-      }
-
       return Promise.all([
         this.openDb(),
         this.get(INTERNAL_STORE_NAME, 'session')
